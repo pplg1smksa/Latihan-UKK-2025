70a77d3c38974d292cf51895c5614c736a5e6ea5
Dani Ramadhan <daniramadhan170907@gmail.com>
2024-08-22 20:39:03 +0700

    menambahkan kode untuk session

--- Coffee/proses.php
+++ Coffee/proses.php
@@ -1,11 +1,18 @@
 <?php
 
+session_start();
+
 $coffeeName[] = $_POST['coffeeName'];
 $coffeeType[] = $_POST['coffeeType'];
 $coffeePrice[] = $_POST['coffeePrice'];
 
-var_dump($coffeeName);
-var_dump($coffeeType);
-var_dump($coffeePrice);
+// Check data
+// var_dump($coffeeName);
+// var_dump($coffeeType);
+// var_dump($coffeePrice);
+
+$_SESSION['coffeeName'] = $coffeeName;
+$_SESSION['coffeeType'] = $coffeeType;
+$_SESSION['coffeePrice'] = $coffeePrice;
 
 ?>
