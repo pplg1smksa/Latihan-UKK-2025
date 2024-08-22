75586acda9fd65c14d4de70c5a15d9edb7415cd2
Dani Ramadhan <daniramadhan170907@gmail.com>
2024-08-22 17:21:45 +0700

    menambahkan struktur html di file index.php

--- Coffee/index.php
+++ Coffee/index.php
@@ -7,6 +7,22 @@
     <title>COFFEE MAENG</title>
 </head>
 <body>
-    
+    <h2>COFFEE MAENG</h2>
+    <div class="container">
+        <form action="proses.php" method="post">
+
+            <label for="coffeeName">Nama Kopi</label>
+            <input type="text" name="coffeeName" required>
+
+            <label for="coffeeType">Tipe Kopi</label>
+            <input type="text" name="coffeeType" required>
+
+            <label for="coffeePrice">Harga</label>
+            <input type="text" name="coffeePrice">
+
+            <button type="submit">Tambah</button>
+            
+        </form>
+    </div>
 </body>
 </html>
