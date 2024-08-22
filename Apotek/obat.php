9b54ca7044bce1c1b07c8ee553edb4ba87fa6e89
Dani Ramadhan <daniramadhan170907@gmail.com>
2024-08-22 16:58:42 +0700

    menambahkan komentar pada obat.php

--- Apotek/obat.php
+++ Apotek/obat.php
@@ -3,6 +3,12 @@
     $jenisObat = $_GET['jenisObat'];
     $hargaObat = $_GET['hargaObat'];
 
+    // Menggunakan var_dump untuk memeriksa apakah data berhasil ditangkap di file ini
+    // var_dump($idObat);
+    // var_dump($jenisObat);
+    // var_dump($hargaObat);
+
+    // Menampilkan data dari hasil input
     echo "ID Obat = " . "<strong>$idObat</strong>" . "<br>";
     echo "Jenis Obat = " . "<strong>$jenisObat</strong>" . "<br>";
     echo "harga Obat = " . "<strong>$hargaObat</strong>" . "<br>";
