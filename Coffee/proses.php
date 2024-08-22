e824082d7426a4a65124a945ea9a3efa07bba311
Dani Ramadhan <daniramadhan170907@gmail.com>
2024-08-22 17:30:39 +0700

    menambahkan kode untuk menangkap data dari file index.php ke file proses.php

---
+++ Coffee/proses.php
@@ -0,0 +1,11 @@
+<?php
+
+$coffeeName[] = $_POST['coffeeName'];
+$coffeeType[] = $_POST['coffeeType'];
+$coffeePrice[] = $_POST['coffeePrice'];
+
+var_dump($coffeeName);
+var_dump($coffeeType);
+var_dump($coffeePrice);
+
+?>
