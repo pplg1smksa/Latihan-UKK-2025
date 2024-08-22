f718630b344e67605c6b92756ff037237b7b02d0
Dani Ramadhan <164002193+Dani170907@users.noreply.github.com>
2024-08-22 16:51:41 +0700

    Add files via upload

---
+++ Apotek/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Apotek</title>
+</head>
+<body>
+    
+    <h2>Apotek</h2>
+
+    <form action="obat.php" method="get"><br>
+        <label for="">Id Obat</label><br>
+        <input type="text" name="idObat"><br>
+        <label for="">Jenis Obat</label><br>
+        <input type="text" name="jenisObat"><br>
+        <label for="">Harga Obat</label><br>
+        <input type="text" name="hargaObat"><br><br>
+        <button type="submit">Simpan</button>
+    </form>
+    
+</body>
+</html>
---
+++ Apotek/obat.php
@@ -0,0 +1,8 @@
+<?php
+    $idObat = $_GET['idObat'];
+    $jenisObat = $_GET['jenisObat'];
+    $hargaObat = $_GET['hargaObat'];
+
+    echo "ID Obat = " . "<strong>$idObat</strong>" . "<br>";
+    echo "Jenis Obat = " . "<strong>$jenisObat</strong>" . "<br>";
+    echo "harga Obat = " . "<strong>$hargaObat</strong>" . "<br>";
