86c1081a8e8cde2ebec819b046d522db5a60034a
Dani Ramadhan <daniramadhan170907@gmail.com>
2024-08-22 21:12:14 +0700

    menampilkan kode menggunakan perulangan

--- Coffee/proses.php
+++ Coffee/proses.php
@@ -27,8 +27,34 @@ $_SESSION['tmpCoffeeName'] = $coffeeName;
 $_SESSION['tmpCoffeeType'] = $coffeeType;
 $_SESSION['tmpCoffeePrice'] = $coffeePrice;
 
-var_dump($_SESSION['tmpCoffeeName']);
-var_dump($_SESSION['tmpCoffeeType']);
-var_dump($_SESSION['tmpCoffeePrice']);
+// var_dump($_SESSION['tmpCoffeeName']);
+// var_dump($_SESSION['tmpCoffeeType']);
+// var_dump($_SESSION['tmpCoffeePrice']);
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COFFEE MAENG</title>
+</head>
+<body>
+    <h2>Daftar Pesanan</h2>
+    <table border="1px" cellpadding="5px">
+        <tr>
+            <th>Nama Kopi</th>
+            <th>Tipe Kopi</th>
+            <th>Harga Kopi</th>
+        </tr>
+        <?php for ($i=0; $i < count($coffeeName) ; $i++) : ?>
+        <tr>
+            <td><?= $coffeeName[$i]; ?></td>
+            <td><?= $coffeeType[$i]; ?></td>
+            <td><?= $coffeePrice[$i]; ?></td>
+        </tr>
+        <?php endfor; ?>
+    </table>
+</body>
+</html>
