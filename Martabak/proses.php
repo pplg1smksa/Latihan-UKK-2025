efbfcee0dcfb7d6a9b22112e8dff6e3384f874f3
Dani Ramadhan <daniramadhan170907@gmail.com>
2024-08-23 20:31:01 +0700

    cek kondisi jika sessian telah tersedia

--- Martabak/proses.php
+++ Martabak/proses.php
@@ -2,6 +2,22 @@
 
 session_start();
 
+if (isset($_SESSION['tmpVarianMartabakManis'])) {
+    $varianMartabakManis = $_SESSION['tmpVarianMartabakManis'];
+}
+
+if (isset($_SESSION['tmpVarianMartabakTelur'])) {
+    $varianMartabakTelur = $_SESSION['tmpVarianMartabakTelur'];
+}
+
+if (isset($_SESSION['tmpPorsiMartabak'])) {
+    $porsiMartabak = $_SESSION['tmpPorsiMartabak'];
+}
+
+if (isset($_SESSION['tmpHargaMartabak'])) {
+    $hargaMartabak = $_SESSION['tmpHargaMartabak'];
+}
+
 $varianMartabakManis = $_GET['varianMartabakManis'];
 $varianMartabakTelur = $_GET['varianMartabakTelur'];
 $porsiMartabak = $_GET['porsiMartabak'];
@@ -23,3 +39,15 @@ $_SESSION['tmpHargaMartabak'] = $hargaMartabak;
 // var_dump($_SESSION['tmpHargaMartabak']);
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
