d7fc4f7fc7146d8ed9ac0f877e44cc4f0bd5b588
Dani Ramadhan <daniramadhan170907@gmail.com>
2024-09-01 05:23:20 +0700

    update file data.php dan data.css

--- Handphone/data.php
+++ Handphone/data.php
@@ -55,6 +55,7 @@ if (isset($_SESSION['tmpHarga'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="data.css">
     <title>Data Pesanan</title>
 </head>
 <body>
@@ -81,5 +82,6 @@ if (isset($_SESSION['tmpHarga'])) {
         <?php $num++;
         endwhile; ?>
     </table>
+    <a href="index.php">Tambah Data</a>
 </body>
 </html>
