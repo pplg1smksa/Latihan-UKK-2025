f9cccbbf266c5fa1cb1d41149ff32eac277a3781
Dani170907 <daniramadhan170907@gmail.com>
2025-02-02 21:47:57 +0700

    add style.css

--- Event/index.php
+++ Event/index.php
@@ -1,20 +1,20 @@
-<?php 
+<?php
 
 $user = 'Admin';
 $pass = 'Admin1234';
 
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
-    if(!empty($username)) {
-        if($username == $user && $password == $pass) {
+    if (!empty($username)) {
+        if ($username == $user && $password == $pass) {
             header('Location: input.php');
         } else {
-            ?> 
-                <script>
-                    alert('Username/Password Salah!')
-                </script>
-            <?php
+?>
+            <script>
+                alert('Username/Password Salah!')
+            </script>
+<?php
         }
     }
 }
@@ -22,21 +22,24 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <div>
+    <div class="container">
         <form action="" method="POST">
             <label for="username">Username: </label>
-            <input type="text" name="username"><br>
+            <input type="text" name="username" required>
             <label for="password">Password: </label>
-            <input type="password" name="password"><br>
+            <input type="password" name="password" required>
             <button type="submit">Masuk</button>
         </form>
     </div>
 </body>
+
 </html>
--- Event/input.php
+++ Event/input.php
@@ -13,23 +13,23 @@ $categories = ['PHBI', 'Kesiswaan', 'Olahraga dan Seni', 'Umum'];
 
 <body>
 
-    <div>
+    <div class="container">
         <form action="table.php" method="post">
             <label for="kodeEvent">Kode Event: </label>
-            <input type="text" name="kodeEvent"><br>
+            <input type="text" name="kodeEvent" required>
             <label for="namaEvent">Name Event: </label>
-            <input type="text" name="namaEvent"><br>
+            <input type="text" name="namaEvent" required>
             <label for="kategori">Kategori: </label>
             <select name="kategori" id="">
                 <option value="" disabled selected>Pilih kategori</option>
                 <?php foreach ($categories as $category) : ?>
                     <option value="<?= $category ?>"><?= $category ?></option>
                 <?php endforeach; ?>
-            </select><br>
+            </select>
             <label for="tanggalEvent">Tanggal Event: </label>
-            <input type="date" name="tanggalEvent"><br>
+            <input type="date" name="tanggalEvent" required>
             <label for="penyelenggara">Penyelenggara: </label>
-            <input type="text" name="penyelenggara"><br>
+            <input type="text" name="penyelenggara" required>
             <button type="submit">Simpan</button>
         </form>
     </div>
