bf2d84c997f121588eb740318f01c7f6430b897e
Dani170907 <daniramadhan170907@gmail.com>
2025-02-02 14:44:26 +0700

    add style.css

--- Event/index.php
+++ Event/index.php
@@ -26,14 +26,17 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form action="" method="POST">
-        <label for="username">Username: </label>
-        <input type="text" name="username"><br>
-        <label for="password">Password: </label>
-        <input type="password" name="password"><br>
-        <button type="submit">Masuk</button>
-    </form>
+    <div>
+        <form action="" method="POST">
+            <label for="username">Username: </label>
+            <input type="text" name="username"><br>
+            <label for="password">Password: </label>
+            <input type="password" name="password"><br>
+            <button type="submit">Masuk</button>
+        </form>
+    </div>
 </body>
 </html>
--- Event/input.php
+++ Event/input.php
@@ -1,31 +1,38 @@
-<?php 
-    $categories = ['PHBI', 'Kesiswaan', 'Olahraga dan Seni', 'Umum'];
+<?php
+$categories = ['PHBI', 'Kesiswaan', 'Olahraga dan Seni', 'Umum'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Input Page</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <form action="table.php" method="post">
-        <label for="kodeEvent">Kode Event: </label>
-        <input type="text" name="kodeEvent"><br>
-        <label for="namaEvent">Name Event: </label>
-        <input type="text" name="namaEvent"><br>
-        <label for="kategori">Kategori: </label>
-        <select name="kategori" id="">
-            <option value="" disabled selected>Pilih kategori</option>
-            <?php foreach($categories as $category) : ?>
-            <option value="<?= $category ?>"><?= $category ?></option>
-            <?php endforeach; ?>
-        </select><br>
-        <label for="tanggalEvent">Tanggal Event: </label>
-        <input type="date" name="tanggalEvent"><br>
-        <label for="penyelenggara">Penyelenggara: </label>
-        <input type="text" name="penyelenggara"><br>
-        <button type="submit">Simpan</button>
-    </form>
+
+    <div>
+        <form action="table.php" method="post">
+            <label for="kodeEvent">Kode Event: </label>
+            <input type="text" name="kodeEvent"><br>
+            <label for="namaEvent">Name Event: </label>
+            <input type="text" name="namaEvent"><br>
+            <label for="kategori">Kategori: </label>
+            <select name="kategori" id="">
+                <option value="" disabled selected>Pilih kategori</option>
+                <?php foreach ($categories as $category) : ?>
+                    <option value="<?= $category ?>"><?= $category ?></option>
+                <?php endforeach; ?>
+            </select><br>
+            <label for="tanggalEvent">Tanggal Event: </label>
+            <input type="date" name="tanggalEvent"><br>
+            <label for="penyelenggara">Penyelenggara: </label>
+            <input type="text" name="penyelenggara"><br>
+            <button type="submit">Simpan</button>
+        </form>
+    </div>
 </body>
+
 </html>
